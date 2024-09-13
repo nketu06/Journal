3537b205ec06398e0ed04f7c570bf9e636d61502
Nishant ketu <nketu06@gmail.com>
2024-09-13 17:27:21 +0530

    added transaction

--- src/main/java/com/nketu/journal/JournalApplication.java
+++ src/main/java/com/nketu/journal/JournalApplication.java
@@ -2,6 +2,9 @@ package com.nketu.journal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
@@ -10,6 +13,10 @@ public class JournalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JournalApplication.class, args);
+	}
+
+	public PlatformTransactionManager mongoTransMangager(MongoDatabaseFactory dbFactory){
+		return new MongoTransactionManager(dbFactory);
 	}
 
 }
